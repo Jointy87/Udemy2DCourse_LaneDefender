3609cc83255c7c72ab76b6a569da08013862394d
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-02-07 18:26:11 +0100

    CREATE: Fox behavior script

---
+++ Assets/Scripts/BehaviorFox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviorFox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject otherObject = other.gameObject;
+
+        if(otherObject.GetComponent<Defender>() && other.tag == "Wall")
+        {
+            Jump();
+        }
+        if (otherObject.GetComponent<Defender>())
+        {
+            GetComponent<Attacker>().Attack(otherObject);
+        }
+    }
+
+    public void Jump()
+    {
+        GetComponent<Animator>().SetTrigger("JumpTrigger");
+    }
+}
