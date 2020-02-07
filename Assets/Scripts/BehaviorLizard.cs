921e16c0fec6b83a9bd193561dde1e5e648f9393
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-02-07 15:47:29 +0100

    ADD: Created

---
+++ Assets/Scripts/BehaviorLizard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviorLizard : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject otherObject = other.gameObject;
+        Debug.Log("Colliding");
+        if (otherObject.GetComponent<Defender>())
+        {
+            GetComponent<Attacker>().PlayAttackAnimation(otherObject);
+            Debug.Log("Got This Far");
+        }
+    }
+}
