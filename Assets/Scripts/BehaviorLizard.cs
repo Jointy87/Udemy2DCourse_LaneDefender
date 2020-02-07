3be43a002aa144c87e0fe406335b23f8c633c7b7
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-02-07 18:26:55 +0100

    EDIT: Rename and removing debugs

--- Assets/Scripts/BehaviorLizard.cs
+++ Assets/Scripts/BehaviorLizard.cs
@@ -7,11 +7,10 @@ public class BehaviorLizard : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         GameObject otherObject = other.gameObject;
-        Debug.Log("Colliding");
+
         if (otherObject.GetComponent<Defender>())
         {
-            GetComponent<Attacker>().PlayAttackAnimation(otherObject);
-            Debug.Log("Got This Far");
+            GetComponent<Attacker>().Attack(otherObject);
         }
     }
 }
